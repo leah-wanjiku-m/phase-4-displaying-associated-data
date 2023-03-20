323dd0e3606e3740cfd9bc27985f734d65948204
leah wanjiku <leagm22@gmail.com>
2023-03-20 09:54:55 +0300

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,12 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def show
+    dog_house = DogHouse.find(params[:id])
+    render json: dog_house
   end
 
 end
